26e162dd500024fa4515b60b762fde1da8aa1c33
Felipe <felipelee22@gmail.com>
2021-09-25 16:00:37 -0300

    middleBar

--- marvel-comic-shop/src/components/Header.vue
+++ marvel-comic-shop/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <TopBar />
-        <div class="middleBar"></div>
+        <MiddleBar />
         <div class="bottomBar"></div>
     </div>
     
@@ -10,11 +10,13 @@
 
 <script>
 import TopBar from './TopBar.vue'
+import MiddleBar from './MiddleBar.vue'
 
 export default {
     name: "Header",
     components: {
-        TopBar
+        TopBar,
+        MiddleBar
     }
 }
 </script>
---
+++ marvel-comic-shop/src/components/MiddleBar.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="middleBar">
+        <div class="container">
+            <img src='../assets/logo-solo.svg' alt="Logo Marvel" />
+            <div class="searchBox">
+                <input type="text" placeholder="Vosso desejo será uma ordem">
+                <button class="btn">Encontre</button>
+            </div>
+            <div class="cart">
+                <a href="#"><img src='../assets/profile-icon.svg' alt="Profile" /></a>
+                <a href="#"><img src='../assets/bag-icon.svg' alt="Checkout" /></a>
+               
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+
+
+export default {
+    name: 'MiddleBar'
+}
+</script>
+
+
+<style scoped>
+.middleBar{
+    background-color: #c70732;
+}
+.container{
+    width: 85%;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.logo{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.container > img{
+    width:20%;
+}
+
+.searchBox {
+    display: flex;
+}
+.searchBox > input{
+    width: 35vw
+}
+.cart{
+    text-align: right;
+}
+
+.cart > a:first-child{
+    margin-right: 1rem;
+}
+.cart > a > img{
+    width:20%;
+}
+</style>
