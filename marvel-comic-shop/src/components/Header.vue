4784b3e2405ece8c50051d5ea531b80a68b63ebd
Felipe <felipelee22@gmail.com>
2021-09-25 14:21:55 -0300

    inicialização do componente header

---
+++ marvel-comic-shop/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="topBar"></div>
+        <div class="middleBar"></div>
+        <div class="bottomBar"></div>
+    </div>
+    
+</template>
+
+
+<script>
+
+
+export default {
+    name: "Header"
+}
+</script>
